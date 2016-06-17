6ce3525133bcc48b98f23e737cafb2829af5f233
Jon Warren <jon@jonwarren.info>
2016-06-17 13:52:04 -0500

    Migrated HomeController functionality to base TasklogsApplication.

--- src/main/java/info/jonwarren/tasklogs/TasklogsApplication.java
+++ src/main/java/info/jonwarren/tasklogs/TasklogsApplication.java
@@ -2,12 +2,35 @@ package info.jonwarren.tasklogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+//@EnableOAuth2Client
 public class TasklogsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TasklogsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TasklogsApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("You are at the start of this whole app.");
+
+        return sb.toString();
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("What? You wanna log in to this or something? Good luck with that!");
+
+        return sb.toString();
+    }
 
 }
