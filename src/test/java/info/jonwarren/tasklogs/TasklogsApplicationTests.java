ca97731f2ae260487fcc4cf3f9593e2e28e8d0e5
Jon Warren <jon@jonwarren.info>
2016-06-17 13:54:09 -0500

    Removed errant, unnecessary, import.

--- src/test/java/info/jonwarren/tasklogs/TasklogsApplicationTests.java
+++ src/test/java/info/jonwarren/tasklogs/TasklogsApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import info.jonwarren.tasklogs.TasklogsApplication;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TasklogsApplication.class)
 @WebAppConfiguration
